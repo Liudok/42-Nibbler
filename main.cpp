353e4bbc2ab7d53950eaa3a2ac8778c95ebb60aa
Nestor <nestoroprysk@gmail.com>
2018-08-08 00:02:44 +0300

    Check of the sampleFunction is added

--- main.cpp
+++ main.cpp
@@ -4,14 +4,20 @@
 
 int main()
 {
+	const auto ncursesDynamicLibName = "libNcursesLib.dylib";
 	const auto ncursesLibHandle =
-		dlopen("libNcursesLib.dylib", RTLD_LAZY | RTLD_LOCAL);
+		dlopen(ncursesDynamicLibName, RTLD_LAZY | RTLD_LOCAL);
 	std::cout << "Library " << (ncursesLibHandle ?
 		"found :)" : "not found :(") << std::endl;
+	if (!ncursesLibHandle) return 1;
 	using sampleFuntionType = void(*)();
+	const auto sampleFunctionName = "sampleFunction";
 	const auto sampleFunctionPtr =
 		reinterpret_cast<sampleFuntionType>(dlsym(ncursesLibHandle,
-			"sampleFunction"));
+			sampleFunctionName));
+	std::cout << "The sample function from the ncurses dynamic library is " <<
+		(sampleFunctionPtr ? "found :)" : "not found") << std::endl;
+	if (!sampleFunctionPtr) return 2;
 	sampleFunctionPtr();
 	std::cout << "Compilation done!: " <<
 		sampleSum(2, 2) << std::endl;
