429ee6373ba1cd1cc74a9d8ee52e2eef2ab18009
Nestor <nestoroprysk@gmail.com>
2018-08-07 23:57:13 +0300

    The dynamic lib is now used without the header and by using dl* functions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "LogicLib/LogicLib.hpp"
-#include "NcursesLib/NcursesLib.hpp"
 #include <dlfcn.h>
 
 int main()
 {
-	sampleFunction();
+	const auto ncursesLibHandle =
+		dlopen("libNcursesLib.dylib", RTLD_LAZY | RTLD_LOCAL);
+	std::cout << "Library " << (ncursesLibHandle ?
+		"found :)" : "not found :(") << std::endl;
+	using sampleFuntionType = void(*)();
+	const auto sampleFunctionPtr =
+		reinterpret_cast<sampleFuntionType>(dlsym(ncursesLibHandle,
+			"sampleFunction"));
+	sampleFunctionPtr();
 	std::cout << "Compilation done!: " <<
 		sampleSum(2, 2) << std::endl;
+	dlclose(ncursesLibHandle);
 }
