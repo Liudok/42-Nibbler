c7b25c3b62a2a7481a4d18e0f3dbb0af7ec22610
Nestor <nestoroprysk@gmail.com>
2018-08-23 02:18:26 +0300

    Sdl is now configured without copying to usr/local

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+BOLD=$(tput bold)
 EOC='\033[0m'
 
 cmakePath=$(command -v cmake)
 if [ "$cmakePath" == "" ]
 then
-	echo "${RED}Please install cmake.${EOC}"
+	echo "${RED}${BOLD}Please install cmake.${EOC}"
 	exit 1
 else
 	echo "${GREEN}Cmake found${EOC}"
@@ -16,18 +17,17 @@ if [ ! -d "$thirdPartiesPath" ]
 then
 	mkdir ThirdParties
 	cd ThirdParties
-	echo "${GREEN}Installing and building third parties...${EOC}"
-	echo "${GREEN}Please wait it may take a while${EOC}"
+	echo "${GREEN}${BOLD}Installing and building third parties...${EOC}"
+	echo "${GREEN}${BOLD}Please wait it may take a while${EOC}"
 	sleep 1
-	
-	git clone https://github.com/nestoroprysk/Sdl2.git
-	(cd /usr/local/opt/ && mkdir sdl2 &&
-		cd sdl2 && mkdir lib);
-	cp Sdl2/lib/libSDL2-2.0.0.dylib /usr/local/opt/sdl2/lib/
-	
+
+	git clone https://github.com/SDL-mirror/SDL.git
+	mkdir SDL/lib
+	(cd SDL/lib && cmake .. && cmake --build .);
+
 	git clone https://github.com/SFML/SFML.git
-	(cd SFML && cmake . && make);
-	echo "${GREEN}Third parties installed${EOC}"
+	(cd SFML && cmake . && cmake --build .);
+	echo "${GREEN}${BOLD}Third parties installed${EOC}"
 	cd ..
 else
 	echo "${GREEN}Third parties found${EOC}"
