48878344606d58ecb55e39d5184e867b3dd249f6
Nestor <nestoroprysk@gmail.com>
2018-09-17 18:56:43 +0300

    Full-screen default in progress

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include "LogicLib/LogicUnit.hpp"
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <stdio.h>
 
 bool validWindowSize(size_t w, size_t h);
+std::string readOutputOfCommand(std::string const& cmnd);
+
+struct S
+{
+    const size_t width;
+    const size_t height;
+};
 
 int main(int ac, char **av)
 {
@@ -30,4 +40,19 @@ int main(int ac, char **av)
 bool validWindowSize(size_t w, size_t h)
 {
     return w > 10 && h > 10 && w < 180 && h < 86;
+}
+
+std::string readOutputOfCommand(std::string const& cmnd)
+{
+    const auto BUFF_SIZE = 32;
+    std::stringstream ss;
+    char buff[BUFF_SIZE];
+    FILE* ret = popen(cmnd.c_str(), "r");
+    while (!feof(ret))
+        if (fgets(buff, BUFF_SIZE, ret))
+            ss << buff;
+    pclose(ret);
+    std::string str(ss.str());
+    str.erase(str.end() - 1);
+    return str;
 }
