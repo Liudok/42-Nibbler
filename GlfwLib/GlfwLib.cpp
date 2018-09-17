5387c9fc0852c57777d2d6d0ff052ff210c3b70d
Liudmyla BEREZYNETS <cqmila@gmail.com>
2018-09-17 18:46:57 +0300

    linker

--- GlfwLib/GlfwLib.cpp
+++ GlfwLib/GlfwLib.cpp
@@ -49,7 +49,7 @@ void GlfwWindow::draw(field const& gameState, size_t score, size_t speed)
     score_ = score;
     speed_ = speed;
     gameStateToPixels(gameState);
-    SDL_RenderClear(renderer_);
+ //   SDL_RenderClear(renderer_);
 }
 
 void GlfwWindow::openWindow(size_t width, size_t height)
@@ -61,7 +61,7 @@ void GlfwWindow::openWindow(size_t width, size_t height)
     if( !glfwInit() )
     {
         fprintf( stderr, "Failed to initialize GLFW\n" );
-        return -1;
+        return ;
     }
 
  //   glfwWindowHint(GLFW_SAMPLES, 4);
@@ -75,19 +75,19 @@ void GlfwWindow::openWindow(size_t width, size_t height)
     if( window_ == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
         glfwTerminate();
-        return -1;
+        return ;
     }
     glfwMakeContextCurrent(window_);
 
     // Initialize GLEW
-    if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "Failed to initialize GLEW\n");
-        glfwTerminate();
-        return -1;
-    }
+//    if (glewInit() != GLEW_OK) {
+//        fprintf(stderr, "Failed to initialize GLEW\n");
+//        glfwTerminate();
+//        return ;
+//    }
 
     // Ensure we can capture the escape key being pressed below
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glfwSetInputMode(window_, GLFW_STICKY_KEYS, GL_TRUE);
 
     // Dark blue background
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
@@ -115,13 +115,13 @@ void GlfwWindow::closeWindow()
 
 void GlfwWindow::showGameOver()
 {
-    SDL_Color color = {199, 50, 176, 0};
-    showText("Game over", (width_ / 2) * 4 - 5, (height_ / 2) * 4, color);
-    color = {59, 150, 116, 0};
-    std::string score("Score: " + std::to_string((int)score_));
-    const char *text = score.c_str();
-    showText(text, (width_ / 2) * 4, (height_ / 2) * 4 + 20, color);
-    SDL_RenderPresent(renderer_);
+//    SDL_Color color = {199, 50, 176, 0};
+//    showText("Game over", (width_ / 2) * 4 - 5, (height_ / 2) * 4, color);
+//    color = {59, 150, 116, 0};
+//    std::string score("Score: " + std::to_string((int)score_));
+//    const char *text = score.c_str();
+//    showText(text, (width_ / 2) * 4, (height_ / 2) * 4 + 20, color);
+//    SDL_RenderPresent(renderer_);
 }
 
 GlfwWindow::~GlfwWindow()
@@ -131,81 +131,82 @@ GlfwWindow::~GlfwWindow()
 
 void GlfwWindow::gameStateToPixels(field const& gameState)
 {
-    std::function<void()> setColor[nbGameFieldCellTypes] = {
-        [this](){ return SDL_SetRenderDrawColor(renderer_, 79, 132, 196, 255); },
-        [this](){ return SDL_SetRenderDrawColor(renderer_, 127, 255, 212, 255); },
-        [this](){ return SDL_SetRenderDrawColor(renderer_, 64, 224, 208, 255); },
-        [this](){ return SDL_SetRenderDrawColor(renderer_, 255, 105, 180, 255); },
-        [this](){ return SDL_SetRenderDrawColor(renderer_, 248, 14, 50, 255); },
-        [this](){ return SDL_SetRenderDrawColor(renderer_, 11, 111, 144, 244); }
-    };
+//    std::function<void()> setColor[nbGameFieldCellTypes] = {
+//        [this](){ return SDL_SetRenderDrawColor(renderer_, 79, 132, 196, 255); },
+//        [this](){ return SDL_SetRenderDrawColor(renderer_, 127, 255, 212, 255); },
+//        [this](){ return SDL_SetRenderDrawColor(renderer_, 64, 224, 208, 255); },
+//        [this](){ return SDL_SetRenderDrawColor(renderer_, 255, 105, 180, 255); },
+//        [this](){ return SDL_SetRenderDrawColor(renderer_, 248, 14, 50, 255); },
+//        [this](){ return SDL_SetRenderDrawColor(renderer_, 11, 111, 144, 244); }
+//    };
     for (size_t i = 0; i < height_; ++i)
     {
         for (size_t j = 0; j < width_; ++j)
         {
-            SDL_Rect rectangle;
-            rectangle.x = (j + 1) * zoomFactor_;
-            rectangle.y = (i + 1) * zoomFactor_;
-            rectangle.w = zoomFactor_;
-            rectangle.h = zoomFactor_;
-            setColor[gameState[i][j]]();
-            SDL_RenderFillRect(renderer_, &rectangle);
+//            SDL_Rect rectangle;
+//            rectangle.x = (j + 1) * zoomFactor_;
+//            rectangle.y = (i + 1) * zoomFactor_;
+//            rectangle.w = zoomFactor_;
+//            rectangle.h = zoomFactor_;
+//            setColor[gameState[i][j]]();
+            gameState[i][j];
+//            SDL_RenderFillRect(renderer_, &rectangle);
         }
     }
-    drawBorders();
-    SDL_RenderPresent(renderer_);
+//    drawBorders();
+//    SDL_RenderPresent(renderer_);
 }
 
 void GlfwWindow::drawBorders()
 {
-    SDL_Rect top;
-    SDL_Rect bottom;
-    SDL_Rect right;
-    SDL_Rect left;
-    SDL_SetRenderDrawColor(renderer_, 95, 158, 160, 255);
-
-    top.x = 0;
-    top.y = 0;
-    top.w = (width_ + 1) * zoomFactor_;
-    top.h = zoomFactor_;
-    SDL_RenderFillRect(renderer_, &top);
-    
-    bottom.x = 0;
-    bottom.y = (height_ + 1) * zoomFactor_;
-    bottom.w = (width_ + 2) * zoomFactor_;
-    bottom.h = zoomFactor_;
-    SDL_RenderFillRect(renderer_, &bottom);
-    
-    right.x = (width_ + 1) * zoomFactor_;
-    right.y = 0;
-    right.w = zoomFactor_;
-    right.h = (height_ + 1) * zoomFactor_;
-    SDL_RenderFillRect(renderer_, &right);
-    
-    left.x = 0;
-    left.y = 0;
-    left.w = zoomFactor_;
-    left.h = (height_ + 1) * zoomFactor_;
-    SDL_RenderFillRect(renderer_, &left);
-
-    const auto score = "score: " + std::to_string(score_);
-    showText(score.c_str(), 0, 0, {199, 50, 176, 0});
-    const auto speed = "speed: " + std::to_string(speed_);
-    showText(speed.c_str(), (width_ / 2.4) * zoomFactor_, 0, {199, 50, 176, 0});
+//    SDL_Rect top;
+//    SDL_Rect bottom;
+//    SDL_Rect right;
+//    SDL_Rect left;
+//    SDL_SetRenderDrawColor(renderer_, 95, 158, 160, 255);
+//
+//    top.x = 0;
+//    top.y = 0;
+//    top.w = (width_ + 1) * zoomFactor_;
+//    top.h = zoomFactor_;
+//    SDL_RenderFillRect(renderer_, &top);
+//
+//    bottom.x = 0;
+//    bottom.y = (height_ + 1) * zoomFactor_;
+//    bottom.w = (width_ + 2) * zoomFactor_;
+//    bottom.h = zoomFactor_;
+//    SDL_RenderFillRect(renderer_, &bottom);
+//
+//    right.x = (width_ + 1) * zoomFactor_;
+//    right.y = 0;
+//    right.w = zoomFactor_;
+//    right.h = (height_ + 1) * zoomFactor_;
+//    SDL_RenderFillRect(renderer_, &right);
+//
+//    left.x = 0;
+//    left.y = 0;
+//    left.w = zoomFactor_;
+//    left.h = (height_ + 1) * zoomFactor_;
+//    SDL_RenderFillRect(renderer_, &left);
+//
+//    const auto score = "score: " + std::to_string(score_);
+//    showText(score.c_str(), 0, 0, {199, 50, 176, 0});
+//    const auto speed = "speed: " + std::to_string(speed_);
+//    showText(speed.c_str(), (width_ / 2.4) * zoomFactor_, 0, {199, 50, 176, 0});
 }
 
-SDL_Rect GlfwWindow::makeRect(size_t x, size_t y, size_t h, size_t w)
-{
-    SDL_Rect result;
-    result.x = x * 2;
-    result.y = y * 2;
-    result.h = h * 2;
-    result.w = w * 2;
-    return result;
-}
-
-void  GlfwWindow::showText(const char *text, size_t x, size_t y, SDL_Color color)
-{
+//SDL_Rect GlfwWindow::makeRect(size_t x, size_t y, size_t h, size_t w)
+//{
+//    SDL_Rect result;
+//    result.x = x * 2;
+//    result.y = y * 2;
+//    result.h = h * 2;
+//    result.w = w * 2;
+//    return result;
+//}
+//
+//void  GlfwWindow::showText(const char *text, size_t x, size_t y, SDL_Color color)
+//{
 //    TTF_Font* font = TTF_OpenFont("NibblerThirdParties/TextFonts/Roboto-Light.ttf", 11);
 //    if (!font) throw std::runtime_error("No font found.");
 //    TTF_SetFontStyle(font, TTF_STYLE_BOLD);
@@ -216,4 +217,4 @@ void  GlfwWindow::showText(const char *text, size_t x, size_t y, SDL_Color color
 //    SDL_DestroyTexture(texture);
 //    SDL_FreeSurface(surface);
 //    TTF_CloseFont(font);
-}
+//}
--- GlfwLib/GlfwLib.hpp
+++ GlfwLib/GlfwLib.hpp
@@ -24,9 +24,9 @@ class GlfwWindow : public IWindow
         void            gameStateToPixels(std::vector<std::vector<size_t>> const& gameState);
         void            drawBorders();
        // Glfw_Rect        makeRect(size_t x, size_t y, size_t h, size_t w);
-        void            showText(const char *text, size_t x, size_t y, Glfw_Color color);
+       // void            showText(const char *text, size_t x, size_t y, Glfw_Color color);
 
-        const size_t    zoomFactor_ = 14;
+     //   const size_t    zoomFactor_ = 14;
         size_t          width_ = 0;
         size_t          height_ = 0;
         GLFWwindow*     window_;
