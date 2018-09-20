4d12f47c5477ecd5bc2ce9834fc9af8c1130a755
Nestor <nestoroprysk@gmail.com>
2018-09-21 00:54:46 +0300

    Updating scripts

---
+++ .cp.sh
@@ -0,0 +1,3 @@
+cp Build/libGlfwLib.dylib . 2> /dev/null
+cp Build/libSdlLib.dylib Build/libSfmlLib.dylib . 2> /dev/null
+cp Build/libSoundLib.dylib Build/Nibbler . 2> /dev/null
--- .fclean.sh
+++ .fclean.sh
@@ -1,14 +1,14 @@
 RED='\033[0;31m'
 CYAN='\033[0;36m'
 
-filesToDelete="$(ls Build/*.dylib 2> /dev/null)"
+filesToDelete="$(ls *.dylib 2> /dev/null)"
 
 sh .clean.sh
 
 buildPath=Build
 if [ -d "$buildPath" ] && [ "$filesToDelete" != "" ]
-then	
-    cd Build && make clean && cd ..
+then
+    sh .rm.sh
     sh .talk.sh "Binaries cleaned" "$CYAN"
 else
     sh .talk.sh "No binaries to delete" "$RED"
